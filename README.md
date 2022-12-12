f3c7d197d9d505d8a389ed6e7403805c825cd56e
Bulikosha <bula86kz@gmail.com>
2022-12-12 19:30:21 +0600

    Updated Context, added entities and made data seeding

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Technologies used:
 - Microsoft.EntityFrameworkCore 6.0.10;
 - Microsoft.EntityFrameworkCore.Tools 6.0.10;
 - Npgsql.EntityFrameworkCore.PostgreSQL 6.0.7;
+- Microsoft.EntityFrameworkCore.Tools.DotNet 2.0.3;
+
+Use the following command in terminal: dotnet tool install --global dotnet-ef --version 6.*. For some reason terminal didn't recognise "dotnet ef" command even with MS EF Tools installed.
+
+Migrations:
+1. To add migration use command: dotnet ef migrations add [name]
+2. Update DataBase: dotnet ef database update
--- TaskTrackerAPI/TaskTrackerAPI/DataContext/Context.cs
+++ TaskTrackerAPI/TaskTrackerAPI/DataContext/Context.cs
@@ -1,11 +1,56 @@
 using Microsoft.EntityFrameworkCore;
+using TaskTrackerAPI.Enums;
+using TaskTrackerAPI.Models;
 
 namespace TaskTrackerAPI.DataContext;
 
 public class Context : DbContext
 {
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ProjectTask> Tasks { get; set; }
     public Context(DbContextOptions<Context> options) : base(options)
     {
         Database.EnsureCreated();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //ManyToOne relation is being configured (many tasks for one project)
+        modelBuilder.Entity<ProjectTask>()
+            .HasOne<Project>(task => task.Project)
+            .WithMany(project => project.Tasks)
+            .HasForeignKey(p => p.ProjectId);
+        
+        //For testing purposes the following initial data seeding is processed
+        modelBuilder.Entity<Project>().HasData(
+            new Project
+            {
+                Id = 1,
+                Name = "Build TEC",
+                StartDate = new DateTime(2022, 12, 15).ToUniversalTime(),
+                CompletionDate = new DateTime(2023, 12, 14).ToUniversalTime(),
+                Status = ProjectStatus.NotStarted,
+                Priority = 1
+            });
+        
+        modelBuilder.Entity<ProjectTask>().HasData(
+            new ProjectTask()
+            {
+                Id = 1,
+                Name = "Prepare documents",
+                Description = "For the purposes of TEC construction Project documents should be prepared",
+                Status = ProjectTaskStatus.InProgress,
+                Priority = 10,
+                ProjectId = 1
+            },
+            new ProjectTask()
+            {
+                Id = 2,
+                Name = "Buq equipments",
+                Description = "Equipments in accordance with Project documentation should be purchased",
+                Status = ProjectTaskStatus.ToDo,
+                Priority = 5,
+                ProjectId = 1
+            });
     }
 }
