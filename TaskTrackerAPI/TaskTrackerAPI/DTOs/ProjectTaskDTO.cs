cfc4979a3ed00559fdaef683c9b89dc78b2ccc82
Bulikosha <bula86kz@gmail.com>
2022-12-12 19:33:14 +0600

    Added data transfer objects to interact with the client, if needed

---
+++ TaskTrackerAPI/TaskTrackerAPI/DTOs/ProjectTaskDTO.cs
@@ -0,0 +1,13 @@
+﻿using TaskTrackerAPI.Enums;
+
+namespace TaskTrackerAPI.DTOs;
+
+public class ProjectTaskDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ProjectTaskStatus Status { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; }
+    public int ProjectId { get; set; }
+}
